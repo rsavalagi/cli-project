8961a9494877e35e3eae47e38d21e34f0baf3a9e
Raghu Savalagi <raghu.savalagi@britive.com>
2022-02-10 00:00:55 +0530

    cli

--- qtool.py
+++ qtool.py
@@ -1,52 +1,94 @@
 import os
+from datetime import timedelta
+
 import click
+import configparser
+import tabulate
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster, ClusterOptions, ClusterTimeoutOptions, QueryOptions
+from couchbase.exceptions import CouchbaseException, ParsingFailedException
 
-user_configs = {}
-
-class Repo:
-    def __init__(self, home=None):
-        self.home = os.path.abspath(home or ".")
-        self.db = None
-
-    def __enter__(self):
-        path = os.path.join(self.home, "repo.db")
-        self.db = connect(path)
-
-    def __exit__(self, exc_type, exc_value, tb):
-        self.db.close()
-
-def save_if_true(ctx, param, value):
-    if not value:
-        ctx.abort()
+APP_NAME = 'qtool'
+BASE_DIR = os.path.realpath(os.path.dirname(__file__))
+CONFIG_PATH = os.path.join(BASE_DIR, '.configs.ini')
+parser = configparser.ConfigParser()
 
 
-@click.group()
-@click.pass_context
-def cli(ctx):
-    """Simple Query Tool to execute N1QL queries on couchbase-server"""
-    ctx.obj = ctx.with_resource(Repo(repo_home))
+def get_config(**kwargs):
+    parser.read(CONFIG_PATH)
+    return parser.get(section=kwargs.get('section'), option=kwargs.get('option'))
+
+
+def set_config(**kwargs):
+    parser.read(CONFIG_PATH)
+    parser.set(kwargs.get("section"), kwargs.get("option"), kwargs.get("value"))
+    with open(CONFIG_PATH, 'w') as configfile:
+        parser.write(configfile, space_around_delimiters=False)
+
+
+def flatten_dict(innerElement, separator='.', prefix=''):
+    return {f"{prefix}{separator}{k}" if prefix else k: v for kk, vv in innerElement.items() for k, v in
+            flatten_dict(vv, separator, kk).items()} if isinstance(innerElement, dict) else {
+        prefix: innerElement}
+
+
+cli = click.Group()
 
 
 @cli.command()
-@click.option('--format', required=True, type=click.Choice(['json', 'table', 'csv']), default='json',
-              prompt="display format: ")
-@click.option('--scope', required=True, default='_default', prompt="scope: ")
-@click.option('--chostname', default='127.0.0.1:8091', required=True, prompt="cluster host: ")
-@click.option('--collection', default='travel-sample', required=True, prompt="collection: ")
-@click.option('--yes', is_flag=True, callback=save_if_true, expose_value=False,
-              prompt='Are you sure you want to save preferences?')
-def configure(chostname, format, scope, collection):
+@click.option('--address', '-a', default='127.0.0.1:8091', required=True, prompt="cluster address or ip ")
+@click.option('--username', '-u', default='Administrator', required=True, prompt="username ")
+@click.option('--password', '-p', required=True, prompt="password ")
+def configure(address, username, password):
     """stores user connection preferences"""
-    user_configs.update({"chostname": chostname, "format": format, "scope": scope, "collection": collection})
-    for k, v in user_configs.items():
-        os.system(f"export {k}={v}")
-        os.system(f"echo ${k}")
+    set_config(section='CLUSTER', option="address", value=address)
+    set_config(section='CLUSTER', option="username", value=username)
+    set_config(section='CLUSTER', option="password", value=password)
+
+    address = get_config(section='CLUSTER', option="address")
+    username = get_config(section='CLUSTER', option="username")
+    password = get_config(section='CLUSTER', option="password")
+
+    click.secho(f"DEBUG: saved configs: {address}, {username}, {password}", fg='green')
+
 
 @cli.command()
-def execute():
-    """helps user execute N1QL query """
-    print("Hello Connect")
-    click.echo(user_configs)
+@click.option('--address', '-a', required=False)
+@click.option('--username', '-u', required=False)
+@click.option('--password', '-p', required=False)
+@click.option('--query', '-q', default='select * from \`travel-simple\`.inventory.airport limit 10', required=True,
+              prompt="query ")
+def execute(address, username, password, query):
+    """executes N1QL query and displays results """
+    if not username:
+        username = get_config(section='CLUSTER', option="username")
+    if not address:
+        address = get_config(section='CLUSTER', option="address")
+    if not password:
+        password = get_config(section='CLUSTER', option="password")
 
-if __name__=="__main__":
-    configure()
+    click.clear()
+
+    connectionSuccess = False
+    try:
+        click.secho(f"CONNECTING TO '{address}'", fg='green')
+        timeout_options = ClusterTimeoutOptions(kv_timeout=timedelta(seconds=5), query_timeout=timedelta(seconds=10))
+        cluster = Cluster.connect(f"couchbase://{address}", ClusterOptions(PasswordAuthenticator(username, password),
+                                                                           timeout_options=timeout_options))
+        connectionSuccess = True
+    except CouchbaseException as ce:
+        click.secho(f"connection failed due to {ce}", fg='red')
+        cluster = None
+
+    if connectionSuccess:
+        try:
+            click.secho(f"{query}", fg='yellow')
+            result = cluster.query(query, QueryOptions(metrics=True))
+
+            rows = [flatten_dict(row) for row in result.rows()]
+            click.secho(tabulate.tabulate(rows, headers='keys', tablefmt='csv', showindex=True), fg='blue')
+
+            click.secho(f"\n{result.metadata().metrics().execution_time()}", fg='yellow')
+
+        except (CouchbaseException, ParsingFailedException) as ex:
+            click.secho(f"failed to parse response due to {ex}")
